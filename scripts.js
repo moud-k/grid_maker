f60fcc1ce14379c5c7e72a0451e1167584eb520f
Mahmoud Kourani <mahmoudkourani@Mahmouds-MBP.lan>
2024-10-13 18:35:23 -0400

    Updated scripts.js to contain function to add a column to grid; updated to index.html to provide functionality to add column button

--- scripts.js
+++ scripts.js
@@ -28,3 +28,30 @@ let addRow = function()
         row_count++;
     }
 }
+
+let addColumn = function()
+{
+    if(column_count == 0)
+    {
+        let new_row = document.createElement("tr");
+        let new_cell = document.createElement("td");
+        let text = document.createTextNode("he");
+        new_cell.appendChild(text);
+        new_row.appendChild(new_cell);
+        table.appendChild(new_row);
+        row_count++;
+        column_count++;
+    }
+    else
+    {
+        let table_rows = document.getElementsByTagName("tr");
+        for(row of table_rows)
+        {
+            let new_cell = document.createElement("td");
+            let text = document.createTextNode("he");
+            new_cell.appendChild(text);
+            row.appendChild(new_cell);
+        }
+        column_count++;
+    }
+}
